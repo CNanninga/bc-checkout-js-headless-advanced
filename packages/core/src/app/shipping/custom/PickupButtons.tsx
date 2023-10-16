68febaac07c202ae04d350c000ce87b38bf88faa
Chris Nanninga <chris.nanninga@bigcommerce.com>
2023-10-16 11:18:27 -0500

    Main logic for PickupButtons

--- packages/core/src/app/shipping/custom/PickupButtons.tsx
+++ packages/core/src/app/shipping/custom/PickupButtons.tsx
@@ -11,6 +11,8 @@ import { withCheckout } from '../../checkout';
 import { CheckoutContextProps } from '@bigcommerce/checkout/payment-integration-api';
 import { SingleShippingFormValues } from '../SingleShippingForm';
 
+import { Button, ButtonVariant } from '../../ui/button';
+
 export interface PickupButtonsState {
     isLoading: boolean;
 }
@@ -36,6 +38,9 @@ export interface WithCheckoutPickupButtonsProps {
 }
 
 class PickupButtons extends Component<PickupButtonsProps & WithCheckoutPickupButtonsProps, PickupButtonsState> {
+    private pickupAddress = this.props.getPickupAddress();
+    private consignments = this.props.getConsignments();
+
     constructor(props: PickupButtonsProps & WithCheckoutPickupButtonsProps) {
         super(props)
   
@@ -43,14 +48,103 @@ class PickupButtons extends Component<PickupButtonsProps & WithCheckoutPickupBut
             isLoading: false,
         }
     }
+
+    handlePickupClick: () => Promise<void> = async () => {
+        const { pickupAddress, props } = this;
+        const {
+            deleteConsignment,
+            getConsignments,
+            handleSingleShippingSubmit,
+            updateShippingAddress,
+            selectShippingOption
+        } = props;
+  
+        this.setState({ isLoading: true });
+        const consignments = getConsignments();
+        if(consignments?.length) {
+            const promises: Promise<CheckoutSelectors>[] = [];
+            consignments?.forEach(({id}) => promises.push(deleteConsignment(id)));
+            await Promise.all(promises);
+        }
+  
+        const state = await updateShippingAddress(pickupAddress);
+        const updatedConsignments = state.data.getConsignments();
+        if(updatedConsignments 
+            && updatedConsignments[0].availableShippingOptions?.length
+        ) {
+            await selectShippingOption(
+                updatedConsignments[0].availableShippingOptions[0].id
+            );
+            await handleSingleShippingSubmit({
+                shippingAddress: pickupAddress,
+                billingSameAsShipping: false,
+                orderComment: "Pickup in-store",
+            });
+        }
+  
+        this.setState({ isLoading: false });
+    }
+
+    handleShipToMeClick: () => Promise<void> = async () => {
+        const { consignments, props } = this;
+        const { updateShippingAddress, getEmptyAddress } = props;
+       
+        const promises: Promise<CheckoutSelectors>[] = [];
+        consignments?.forEach(
+            () => promises.push(updateShippingAddress(getEmptyAddress()))
+        );
+        if(promises.length) {
+            this.setState({ isLoading: true });
+            await Promise.all(promises);
+            this.setState({ isLoading: false });
+        }  
+    }
   
     render(): ReactNode {
-        const { isLoading } = this.state
+        const { isLoading } = this.state;
+        const {
+            isPickup,
+            getConsignments
+        } = this.props;
+        const consignments = getConsignments();
+        const pickup = isPickup(consignments ? consignments : []);
   
-        if(isLoading) {
+        if (isLoading) {
             return <div>Loading...</div>
+        } else if (pickup) {
+            return (
+                <div>
+                    <h3>Currently Picking Up In-Store</h3>
+                    <div>Choose an option below</div>
+                    <Button
+                        variant={ButtonVariant.Primary}
+                        onClick={this.handlePickupClick}>
+                        Continue with Pickup
+                    </Button>
+                    <span>&nbsp;-- or --&nbsp;</span>
+                    <Button
+                        variant={ButtonVariant.Primary}
+                        onClick={this.handleShipToMeClick}>
+                        Ship to Me
+                    </Button>
+                </div>
+            )
         } else {
-            return <div>Hello World</div>
+            return (
+                <div>
+                    <h3>Currently Shipping to You</h3>
+                    <div>
+                        Enter your shipping address, 
+                        or click the button below to pickup 
+                        in store instead.
+                    </div>
+                    <Button
+                        variant={ButtonVariant.Primary}
+                        onClick={this.handlePickupClick}>
+                        Pickup In-Store
+                    </Button>
+                </div>
+            )
         }
     }
 }
